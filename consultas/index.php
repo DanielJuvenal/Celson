953328fc1ecddf2f1da5e7e1efbcae87096e4cd5
Daniel Juvenal <119390208+DanielJuvenal@users.noreply.github.com>
2022-12-07 16:59:02 -0300

    celson

--- consultas/index.php
+++ consultas/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Consultas</title>
     <?php
     
     
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="Consultas.css">
 </head>
 
-<body>
+<body id=corp>
 <div id="formulario">
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
 <input type="text" name="Nome" lenght="10" >
